d00c2a8d85b47d6314ed5347e0f8205ecd4174ea
Allan Carvalho <allancarv.almeida@gmail.com>
2025-09-10 17:49:11 -0300

    bug fopen

--- sequence.c
+++ sequence.c
@@ -27,7 +27,7 @@ void initMatrix(float matrix[size2][size])
 void copyMatrix(float matrix[size2][size])
 {
 
-    FILE *f = fopen("linear10.dat", "r");
+    FILE *f = fopen("/home/allan/Documentos/Programação/C/projetoPCP/linear10.dat", "r");
     if (f == NULL)
     {
         perror("Error opening file");
@@ -49,7 +49,7 @@ void calculateResultX(float matrix[size2][size], float result[size])
 {
     int verify = 0;
     float buffer[size];
-
+    printf("oláaaa");
     initVet(buffer);
 
     do
@@ -74,19 +74,25 @@ void calculateResultX(float matrix[size2][size], float result[size])
             result[i] = buffer[i];
 
     } while (verify == 1);
+    printf("oláaaa2");
 
     return;
 }
 
 int main()
 {
+    printf("oi4");
+
     float matrix[size2][size];
     float result[size];
 
     initMatrix(matrix);
     initVet(result);
+    printf("oi1");
     copyMatrix(matrix);
+    printf("oi2");
     calculateResultX(matrix, result);
+    printf("oi3");
 
     for (int i = 0; i < size; i++)
         printf("X%d = %.4f\n", i + 1, result[i]);
